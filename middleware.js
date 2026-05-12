fbac73bcf49444ce4a7aa6a51e8b57c173a785cd
Anshul Sharma <anshul031003@gmail.com>
2026-05-12 19:54:00 +0530

    fix clerk version and middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']) // Add more routes as needed
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-}) // Protect all routes by default 
-
+export default authMiddleware({
+  publicRoutes: ["/", "/how-it-works", "/about-us"],
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
